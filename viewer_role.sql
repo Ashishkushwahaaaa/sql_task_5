c4284f4c5a40ccc3d81cb6c24a07babb68d680cd
Ashishkushwahaaaa <ashishkushwahaaaa@gmail.com>
2025-09-18 00:02:51 +0530

    modified

--- viewer_role.sql
+++ viewer_role.sql
@@ -1,3 +1,3 @@
 SELECT USER();
-SELECT * FROM products;  -- ✅ works
-INSERT INTO products (product_name, price, stock) VALUES ('Tablet', 500, 5); -- ❌ should fail
+SELECT * FROM products;  -- works
+INSERT INTO products (product_name, price, stock) VALUES ('Tablet', 500, 5); -- should fail
